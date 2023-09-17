a3126e1ef42df132d6128089073ddd5918bade76
Fardjad Davari <public@fardjad.com>
2023-09-17 18:00:47 +0200

    Reexport everything from commander

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { InteractiveCommand } from "./interactive-command.ts";
 export { InteractiveOption } from "./interactive-option.ts";
-export { Option } from "commander";
+export * from "commander";
 export { type RegisterFunction } from "./command-plugin.ts";
