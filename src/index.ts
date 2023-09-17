ef5e408c7fbdb577bc255c9fee0a453651b94499
Fardjad Davari <public@fardjad.com>
2023-09-17 17:42:44 +0200

    Export RegisterFunction type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { InteractiveCommand } from "./interactive-command.ts";
 export { InteractiveOption } from "./interactive-option.ts";
 export { Option } from "commander";
+export { type RegisterFunction } from "./command-plugin.ts";
